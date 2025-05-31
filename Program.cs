d1c3f2b48b05e403dc9d93886a9c7f72d2b893da
Lucas526-jpg <Lucasfrx432@gmail.com>
2025-05-31 19:38:15 -0300

    Ejercicio 1 casi finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+        Console.Write("Ingrese un numero");
+        string Entrada= Console.ReadLine() ?? "";
+        if (int.TryParse(Entrada,out int numero))
+        {
+            int nuevoNum = 0;
+            while (numero != 0)
+            {
+                int digito = numero%10;
+                nuevoNum=nuevoNum*10 + digito;
+                numero = numero/10;
+            }
+            Console.WriteLine($"el numero resultante es: {nuevoNum}");
+        }else
+        {
+            Console.Write("Error");
+        }
+    }
+}
