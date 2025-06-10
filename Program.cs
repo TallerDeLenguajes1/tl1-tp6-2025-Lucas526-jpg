5cd2c1319dff8adb371a7be32acdd4b24c943498
Lucas526-jpg <Lucasfrx432@gmail.com>
2025-06-10 08:37:18 -0300

    ultimos pasos antes del mergue en ambas calculadoras

--- Program.cs
+++ Program.cs
@@ -12,6 +12,20 @@ class Program{
             Console.WriteLine($"Raiz cuadrada: {Math.Sqrt(numero)}");
             Console.WriteLine($"Seno: {Math.Sin(numero)}");
             Console.WriteLine($"Coseno: {Math.Cos(numero)}");
+
+            int parteEntera = (int)Math.Truncate(numero);
+            Console.WriteLine($"Parte entera: {parteEntera}");
+
+            Console.WriteLine("Ingrese el numero a");
+            string Num_a=Console.ReadLine() ?? "";
+            double.TryParse(Num_a,out double numero1);
+
+            Console.WriteLine("Ingrese el numero b");
+            string Num_b=Console.ReadLine() ?? "";
+            double.TryParse(Num_b,out double numero2);
+
+            Console.WriteLine($"Maximo: {Math.Max(numero1, numero2)}");
+            Console.WriteLine($"Minimo: {Math.Min(numero1, numero2)}");
         }else
         {
             Console.Write("Error");
