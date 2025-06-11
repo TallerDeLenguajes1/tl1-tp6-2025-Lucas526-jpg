7a99f5c1c93302cb2f984ee6f6d3bd645603b797
Lucas526-jpg <Lucasfrx432@gmail.com>
2025-06-10 22:58:44 -0300

    Ultimo ejercicio casi terminado

--- Program.cs
+++ Program.cs
@@ -2,21 +2,46 @@
 
 class Program{
     static void Main(){
-        Console.Write("Ingrese un numero");
-        string Entrada= Console.ReadLine() ?? "";
-        if (int.TryParse(Entrada,out int numero))
+        //Obtener la longitud de la cadena y muestre por pantalla. 
+        Console.WriteLine("Ingrese un texto");
+        string cadena = Console.ReadLine();
+        int longitud = cadena.Length;
+        Console.WriteLine($"Tamaño: {longitud}");
+
+        //A partir de una segunda cadena ingresada por el usuario, concatene ambas cadenas distintas
+        Console.WriteLine("Ingrese un texto2");
+        string cadena2 = Console.ReadLine();
+        string concatena = cadena + " " +  cadena2;
+        Console.WriteLine($"Texto concatenado: {concatena}");
+
+        //Extraer una subcadena de la cadena ingresada
+        string sub1 = cadena2.Substring(2);
+        Console.WriteLine($"subcadena: {sub1}");
+
+        //Utilizando la calculadora creada anteriormente realizar las operaciones de dos números y mostrar por pantalla y mostrar en texto el resultado. Por ejemplo para la suma sería:  “la suma de “ num1 “ y de” num2 “ es igual a: ” resultado.  Donde num1, num2 y resultados son los sumandos y el resultado de la operación respectivamente. Nota: Busque el comportamiento del Método ToString(); 
+
+        // Recorrer la cadena de texto con un ciclo Foreach e ir mostrando elemento por elemento en pantalla 
+        foreach (char c in concatena)
         {
-            int nuevoNum = 0;
-            while (numero != 0)
-            {
-                int digito = numero%10;
-                nuevoNum=nuevoNum*10 + digito;
-                numero = numero/10;
-            }
-            Console.WriteLine($"el numero resultante es: {nuevoNum}");
+            Console.WriteLine(c);
+        } 
+
+        //Buscar la ocurrencia de una palabra determinada en la cadena ingresada 
+        Console.WriteLine("Ingrese la palabra a buscar");
+        if (concatena.Contains(Console.ReadLine()))
+        {
+            Console.WriteLine("La palabra ingresada esta en la frase");
         }else
         {
-            Console.Write("Error");
+            Console.WriteLine("La palabra ingresada no existe en la frase");
         }
+
+        //Convierta la cadena a mayúsculas y luego a minúsculas. 
+        Console.WriteLine($"La frase en mayusculas{concatena.ToUpper()}");
+        Console.WriteLine($"La frase en mayusculas{concatena.ToLower()}");
+        
+    
+
+     
     }
 }
